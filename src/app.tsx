8981c4ec44b3c8da34e5cc062a0de509d9aba7aa
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2024-09-11 23:51:52 -0300

    Add files via upload

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+import { Summary } from "./components/summary";
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
+
+export function App() {
+  const [summary, setSummary] = useState(null);
+
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 1 minute
+  });
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal />
+    </Dialog>
+  );
+}
