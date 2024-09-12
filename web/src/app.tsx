f02eeb930de9bc6c06026c379564f088840ff7e6
henriquecolef <henriquecolefernandes@gmail.com>
2024-09-12 20:38:08 -0300

    chore: removed unused state and verification for null data

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,13 +2,10 @@ import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { EmptyGoals } from "./components/empty-goals";
 import { Summary } from "./components/summary";
-import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getSummary } from "./http/get-summary";
 
 export function App() {
-  const [summary, setSummary] = useState(null);
-
   const { data } = useQuery({
     queryKey: ["summary"],
     queryFn: getSummary,
